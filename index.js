d7006f051a006a951106b3f7139e3852dfd83e0e
tyler jones <rteyl1690@gmail.com>
2020-08-13 21:52:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
 
-  if (right > 0) {
+  if (right < 180) {
     dodger.style.right = `${left + 1}px`;
   }
 }
